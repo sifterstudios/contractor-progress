fef0be790b2b5986b2b0c59e4917d9dbb49621ac
Tormod Liseth <tormod.liseth@klp.no>
2023-07-21 20:37:17 +0200

    Add Data Management stub

---
+++ src/data/data_management.go
@@ -0,0 +1,12 @@
+package data
+
+var weeks = make(map[string]Week)
+var contractorGoal Goal
+
+func addWeek(weekNumber string, week Week) {
+	weeks[weekNumber] = week
+}
+
+func setContractorGoal(goal Goal) {
+	contractorGoal = goal
+}
